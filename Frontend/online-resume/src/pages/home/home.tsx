4a48a28b47a2fed27e4613d41beda91a1917176d
GerritBurger <gerrit.burger@impact.com>
2022-09-19 15:23:50 +0200

    [Frontend] Update Top Section Home Page

--- Frontend/online-resume/src/pages/home/home.tsx
+++ Frontend/online-resume/src/pages/home/home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./home.scss";
-import companyLogo from "../../assets/images/company-logo.png";
+import myAvatar from "@/assets/images/my-avatar.jpeg";
+import companyLogo from "@/assets/images/company-logo.png";
 import IconCard from "@/components/icon-card/icon-card";
 
 const Home = () => {
@@ -8,7 +9,26 @@ const Home = () => {
         <div className="home-page">
             <div className="top-section">
                 <div className="my-bio">
-                    <h1>This is the home page</h1>
+                    <div className="image-container">
+                        <img src={myAvatar} alt="my-avatar"/>
+                    </div>
+
+                    <div className="content-container">
+                        <h1>Who am I?</h1>
+
+                        <p>TLDR; a good software engineer and a hard worker.</p>
+
+                        <p>
+                            On a serious note, I'm a mid-level software engineer specialising in Go and Typescript (React) and
+                            I'm very passionate about development and learning new things.
+                        </p>
+
+                        <p>
+                            Why do I do this? I get an overwhelming sense of satisfaction from working with a team to build
+                            out projects and seeing it flourish. Software development is not only my job but also a hobby which
+                            means I get up every morning feeling excited thinking about today's challenges!
+                        </p>
+                    </div>
                 </div>
 
                 <div className="current-employment-container">
