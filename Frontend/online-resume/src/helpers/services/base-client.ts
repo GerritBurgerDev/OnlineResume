95a3c89f9ff82955532a98b7b5854c2a3165c95b
GerritBurger <gerrit.burger@impact.com>
2022-09-25 17:14:53 +0200

    [Prod Changes]: Update the BASE_URL

--- Frontend/online-resume/src/helpers/services/base-client.ts
+++ Frontend/online-resume/src/helpers/services/base-client.ts
@@ -11,7 +11,7 @@ interface IBaseClient {
 }
 
 export class BaseClient implements IBaseClient {
-    private BASE_URL = 'http://localhost:8081/';
+    private readonly BASE_URL;
     private TIMEOUT = 10000; // 10 seconds
     private apiConfig: ApiConfig | undefined;
     private client: AxiosInstance;
@@ -30,6 +30,8 @@ export class BaseClient implements IBaseClient {
         // TODO: I need to make this better.
         if (!window.location.href.includes('localhost')) {
             this.BASE_URL = 'http://ec2-18-169-241-12.eu-west-2.compute.amazonaws.com:8081';
+        } else {
+            this.BASE_URL = 'http://localhost:8081/';
         }
 
         this.apiConfig = apiConfig;
