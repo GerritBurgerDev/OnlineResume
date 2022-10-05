ddf3387865e34a6f816e3c030fc2ad1fe5b99624
GerritBurger <gerrit.burger@impact.com>
2022-10-05 14:26:56 +0200

    [Backend]: update project recommendations url

--- Backend/main.go
+++ Backend/main.go
@@ -25,12 +25,12 @@ func handleRequests() {
 	router.HandleFunc("/projects", GetAllProjects).Methods("GET")
 	router.HandleFunc("/projects/{skill}", GetProjectsForSkill).Methods("GET")
 	router.HandleFunc("/project/{id}", GetProject).Methods("GET")
+	router.HandleFunc("/project/{id}/recommendations", GetRecommendationForProject).Methods("GET")
 	router.HandleFunc("/recommendations", GetAllRecommendations).Methods("GET")
 	router.HandleFunc("/recommendations", AddRecommendation).Methods("POST")
 	router.HandleFunc("/recommendations", UpdateRecommendationState).Methods("PUT")
 	router.HandleFunc("/recommendation/{id}", RemoveRecommendation).Methods("DELETE")
 	router.HandleFunc("/recommendation/{id}", GetRecommendation).Methods("GET")
-	router.HandleFunc("/project/{projectId}/recommendations", GetRecommendationForProject).Methods("GET")
 
 	cors := cors.New(cors.Options{
 		AllowedOrigins: []string{
